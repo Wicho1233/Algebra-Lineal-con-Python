9e283ec5fca66e287134bcef783f05f34a652206
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-03-14 22:30:39 -0600

    Create AlgebraLineal1.py

---
+++ AlgebraLineal1.py
@@ -0,0 +1,11 @@
+#Espacios Vectoriales
+import numpy as np
+#Importacion de la libreria numpy que permite trabajar con arreglos
+#Declaracion del primer vector 'a'
+a=np.array([-1,9,5,2])
+#Declaracion del segundo vector 'b'
+b=np.array([1,3,-1,7])
+#Manera de realiza la suma de los vectores
+result_sum = np.add(a,b)
+print(result_sum)
+
