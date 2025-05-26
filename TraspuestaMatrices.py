808831831760b83faf040f97a8d25ad2f193fadc
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-05-26 07:18:38 -0600

    Traspuesta de una matriz usando Numpy

---
+++ TraspuestaMatrices.py
@@ -0,0 +1,6 @@
+import numpy as np
+#Declaracion de la matriz
+A = np.array([[1,2,3,4],[4,3,2,1],[3,2,1,2],[1,6,8,2]])
+#Manera de resolver la traspuesta de una matriza con la funcion transpose
+result =np.transpose(A)
+print(result)
